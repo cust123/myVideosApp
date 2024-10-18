67b50a6d5a920e1eaa2f82593e417f0f2aa06b07
Muhammad Shafiq <shafiqpesh123@gmail.com>
2024-10-18 17:44:08 +0500

    check the status

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,10 +1,10 @@
-const asyncHanlder = (requestHandler) => {
-  (req, res, next) => {
+const asyncHandler = (requestHandler) => {
+  return (req, res, next) => {
     Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
   };
 };
 
-export { asyncHanlder };
+export { asyncHandler };
 
 // one way of writing the async handler function
 
