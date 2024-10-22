b4120a24df3ab5163007c51967313c9be549654d
Muhammad Shafiq <shafiqpesh123@gmail.com>
2024-10-22 16:37:32 +0500

    first controller for user

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,9 +1,26 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
 
+import { ApiError } from "../utils/apiError.js";
+import { User } from "../models/user.model.js";
+
 const registerUser = asyncHandler(async (req, res) => {
-  res.status(200).json({
-    message: "ok",
-  });
+  const { fullname, email, username, password } = req.body;
+
+  if (
+    [fullname, username, email, password].some((field) => field?.trim() === "")
+  ) {
+    throw new ApiError(400, "All Fields are Required!");
+  }
+
+  const existedUser = User.findOne({ $or: [{ username }, { email }] });
 });
 
+if (existedUser) {
+  throw new ApiError(409, "User with email or username already exist");
+}
+
 export { registerUser };
+
+// if (fullname === "") {
+//   throw new apiError(400, "full name is required");
+// }
