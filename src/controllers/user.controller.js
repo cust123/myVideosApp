b86a69864488e1e901e9d24e825653f151414782
Muhammad Shafiq <shafiqpesh123@gmail.com>
2024-10-29 17:46:33 +0500

    new changes

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -60,7 +60,18 @@ const registerUser = asyncHandler(async (req, res) => {
     .json(new apiResponse(201, createdUser, "user registered successfully!"));
 });
 
-export { registerUser };
+const loginUser = asyncHandler(async (req, res) => {
+  const { email, username, password } = req.body;
+  if (!username || !email) {
+    throw new ApiError(400, "User Name or Email is required");
+  }
+
+  const user = await User.findOne({
+    $or: [{ username }, { email }],
+  });
+});
+
+export { registerUser, loginUser };
 
 // if (fullname === "") {
 //   throw new apiError(400, "full name is required");
