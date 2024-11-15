e88201446d5d0bbee2f4cbc3826f132e43c3532d
Muhammad Shafiq <shafiqpesh123@gmail.com>
2024-11-15 19:18:31 +0500

    subscription controller boiler plate completed

---
+++ src/models/subscription.model.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+import { Schema } from "mongoose";
+
+const subscriptionSchema = new Schema(
+  {
+    subscriber: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  { timestamps: true }
+);
+
+export const Subscription = mongoose.model("Subscription", subscriptionSchema);
