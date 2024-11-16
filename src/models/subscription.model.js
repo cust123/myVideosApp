619bba2e64b31d1e8696734aa4bd2fb59589bc52
Muhammad Shafiq <shafiqpesh123@gmail.com>
2024-11-16 16:18:39 +0500

    controllers for password change

--- src/models/subscription.model.js
+++ src/models/subscription.model.js
@@ -4,7 +4,7 @@ import { Schema } from "mongoose";
 const subscriptionSchema = new Schema(
   {
     subscriber: {
-      type: Schema.Types.ObjectId,
+      type: Schema.Types.ObjectId, //  this user who is subscribing
       ref: "User",
     },
   },
